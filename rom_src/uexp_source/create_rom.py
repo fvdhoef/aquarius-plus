f1c686639306e83abc06688e38bfe827d08d6a04
Frank van den Hoef <fvdhoef@icloud.com>
2022-02-18 18:51:23 +0100

    Adding CP437 font.

--- rom_src/uexp_source/create_rom.py
+++ rom_src/uexp_source/create_rom.py
@@ -1,23 +1,30 @@
 #!/usr/bin/env python3
 
-with open("../../assets/aquarius_s2.rom", "rb") as f:
-    stockrom = bytearray(f.read())
-
-with open("zout/aqubasic.cim", "rb") as f:
-    extrom = f.read()
-
-# Patch stock ROM
-
-# Cold start entry point in ROM
-stockrom[0x010F] = 0xC3   # JP $2000
-stockrom[0x0110] = 0x00
-stockrom[0x0111] = 0x20
-
-# Warm start entry point in ROM
-stockrom[0x00ED] = 0xC3   # JP $2003
-stockrom[0x00EE] = 0x03
-stockrom[0x00EF] = 0x20
-
 with open("../../aquarius.rom", "wb") as f:
-    f.write(stockrom)
-    f.write(extrom)
+    # Patched Aquarius S2 ROM
+    with open("../../assets/aquarius_s2.rom", "rb") as f2:
+        stockrom = bytearray(f2.read())
+
+        # Cold start entry point in ROM
+        stockrom[0x010F] = 0xC3   # JP $2000
+        stockrom[0x0110] = 0x00
+        stockrom[0x0111] = 0x20
+
+        # Warm start entry point in ROM
+        stockrom[0x00ED] = 0xC3   # JP $2003
+        stockrom[0x00EE] = 0x03
+        stockrom[0x00EF] = 0x20
+
+        f.write(stockrom)
+
+    # Extended ROM
+    with open("zout/aqubasic.cim", "rb") as f2:
+        f.write(f2.read())
+
+    # Aquarius Character ROM
+    with open("../../assets/AquariusCharSetS2.bin", "rb") as f2:
+        f.write(f2.read())
+
+    # CP437 Character ROM
+    with open("../../assets/font8x8.bin", "rb") as f2:
+        f.write(f2.read())
