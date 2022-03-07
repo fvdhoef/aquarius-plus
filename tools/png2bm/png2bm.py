474f28426dc66b5aa50f31f29c18f23d4087ad62
Frank van den Hoef <fvdhoef@icloud.com>
2022-03-07 19:39:04 +0100

    Image converter.

---
+++ tools/png2bm/png2bm.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+import PIL.Image
+
+image = PIL.Image.open("middleearth_b_ntsc.png")
+
+if image.width != 320 or image.height != 200:
+    print("Image must be 320x200")
+    exit(1)
+
+image = image.convert(
+    mode="P", palette=PIL.Image.ADAPTIVE, dither=PIL.Image.FLOYDSTEINBERG
+)
+
+palette = image.getpalette()
+palette = [palette[3 * n : 3 * n + 3] for n in range(256)][0 : len(image.getcolors())]
+if len(palette) > 16:
+    print("Too many colors in picture")
+    exit(1)
+
+palette = [
+    (entry[0] >> 6) | ((entry[1] >> 6) << 2) | ((entry[2] >> 6) << 4)
+    for entry in palette
+]
+palette = palette + (16 - len(palette)) * [0]
+
+print(palette)
+
+
+# Generate color RAM
+colorram_data = []
+for row in range(25):
+    for column in range(40):
+        colors = set()
+
+        for y in range(8):
+            for x in range(8):
+                colors.add(image.getpixel((column * 8 + x, row * 8 + y)))
+
+        colors = list(colors)
+        if len(colors) == 1:
+            colors = [colors[0], colors[0]]
+        if len(colors) != 2:
+            print(f"More than 2 colors ({len(colors)}) in row {row} column {column}")
+
+        colorram_data.append(colors)
+
+colorram = bytearray([(colors[0] << 4 | colors[1]) for colors in colorram_data])
+
+# Generate bitmap RAM
+bitmapram = bytearray()
+for line in range(200):
+    for column in range(40):
+        colors = colorram_data[line // 8 * 40 + column]
+
+        bm = 0
+        for i in range(8):
+            idx = colors.index(image.getpixel((column * 8 + i, line)))
+            if idx == 0:
+                bm |= 1 << (7 - i)
+
+        bitmapram.append(bm)
+bitmapram = bitmapram.ljust(8192, b"\x00")
+
+with open("image.bin", "wb") as f:
+    f.write(bitmapram)
+    f.write(colorram)
