01cee4f3f3b015886c6520c53eaaf6820a1cddfb
Frank van den Hoef <fvdhoef@icloud.com>
2022-05-12 19:19:31 +0200

    Adding copyright notice to bas2txt.py and txt2bas.py

--- tools/bas2txt.py
+++ tools/bas2txt.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
+#-----------------------------------------------------------------------------
+# Copyright (C) 2022 Frank van den Hoef
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE
+#-----------------------------------------------------------------------------
 
 import argparse
 import struct
@@ -8,13 +32,7 @@ parser = argparse.ArgumentParser(
     description="Convert Aquarius BASIC .BAS file to text file"
 )
 parser.add_argument("input", help="Input file")
-parser.add_argument(
-    "-o",
-    "--output",
-    help="Output file",
-    type=argparse.FileType("w"),
-    default=sys.stdout,
-)
+parser.add_argument("output", help="Output file", type=argparse.FileType("w"))
 
 args = parser.parse_args()
 
