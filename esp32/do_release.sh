fa689fbfc926a09f950b4ce4bd4e5efd1f606358
Frank van den Hoef <fvdhoef@icloud.com>
2023-04-20 07:15:27 +0200

    Add missing file to release script.

--- esp32/do_release.sh
+++ esp32/do_release.sh
@@ -2,9 +2,10 @@
 idf.py build
 
 mkdir -p $1 $1/bootloader $1/partition_table
+cp build/bootloader/bootloader.bin $1/bootloader/
 cp build/aquarius-plus.bin $1/
 cp build/partition_table/partition-table.bin $1/partition_table/
-cp build/bootloader/bootloader.bin $1/bootloader/
+cp build/ota_data_initial.bin $1/
 cp build/flash_args $1/
 echo "esptool.py --chip esp32s2 -p COM3 write_flash `cat build/flash_args | tr '\n' ' '`" > $1/write.bat
 unix2dos $1/write.bat
