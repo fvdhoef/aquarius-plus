b865c541b2ddaead77f12a2e4c446ac845c61efc
Frank van den Hoef <fvdhoef@icloud.com>
2023-07-11 16:01:38 +0200

    Update release script for ESP32-S3.

--- esp32/do_release.sh
+++ esp32/do_release.sh
@@ -7,7 +7,7 @@ cp build/aquarius-plus.bin $1/
 cp build/partition_table/partition-table.bin $1/partition_table/
 cp build/ota_data_initial.bin $1/
 cp build/flash_args $1/
-echo "esptool.py --chip esp32s2 -p COM3 write_flash `cat build/flash_args | tr '\n' ' '`" > $1/write.bat
+echo "esptool.py --chip esp32s3 -p COM3 write_flash `cat build/flash_args | tr '\n' ' '`" > $1/write.bat
 unix2dos $1/write.bat
 zip -rv $1.zip $1
 rm -rf $1
