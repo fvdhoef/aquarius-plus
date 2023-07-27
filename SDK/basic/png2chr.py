25d41aebca0c3ae1f734b425aed7a3403a9246cb
Frank van den Hoef <fvdhoef@icloud.com>
2023-07-27 17:52:01 +0200

    Fixing character set conversion script.

--- SDK/basic/png2chr.py
+++ SDK/basic/png2chr.py
@@ -15,23 +15,15 @@ if image.width != 128 or image.height != 128:
     print("Image must be 128x128")
     exit(1)
 
-# Turn into black/white bitmap, if not already that way
-image = image.convert('L')
-
 # Generate bitmap RAM
 bitmapram = bytearray()
-for line in range(128):
-    for column in range(16):
-        myBits = [0] * 8
-        myByte = 0
-        for pixel in range(8):
-            coordinate = x, y = (column % 16) * 8 + pixel, (column // 16) * 8 + line
-            if image.getpixel(coordinate)[0] > 0:
-                myBits[pixel] = 1
-            myByte |= myBits[pixel]
-            myByte <<= 1
-        bitmapram.append(myByte)
 
-bitmapram = bitmapram.ljust(2048, b"\x00")
+for k in range(256):
+    for j in range(8):
+        val = 0
+        for i in range(8):
+            if image.getpixel(((k % 16) * 8 + i, (k // 16) * 8 + j))[0] > 0:
+                val |= (1<<(7-i))
+        bitmapram.append(val)
 
 args.output.write(bitmapram)
