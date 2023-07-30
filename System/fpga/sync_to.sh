fa91d0b81cb17c68446a4f36ce18fd9bfb6c3b44
Frank van den Hoef <fvdhoef@icloud.com>
2023-07-30 09:01:21 +0200

    Enabling pull-ups on datalines.

--- System/fpga/sync_to.sh
+++ System/fpga/sync_to.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd "${0%/*}"
-rsync --include='**.gitignore' --exclude='.git/' --exclude='*.vcd' --filter=':- .gitignore' --progress -a ./ server:Work/aquarius-plus/fpga/
+rsync --delete --include='**.gitignore' --exclude='.git/' --exclude='*.vcd' --filter=':- .gitignore' --progress -a ./ server:Work/aquarius-plus/fpga/
