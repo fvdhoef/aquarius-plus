6992d4a3717db3f498e948e5bd77e14b888f7d5e
Frank van den Hoef <fvdhoef@icloud.com>
2023-08-12 14:58:41 +0200

    Allow ESP to soft-update system ROM.

--- System/fpga/src/top.v
+++ System/fpga/src/top.v
@@ -56,9 +56,10 @@ module top(
     output wire        esp_notify
 );
 
-    wire [7:0] spibm_wrdata;
-    wire       spibm_wrdata_en;
-    wire       spibm_en;
+    wire [15:0] spibm_a;
+    wire  [7:0] spibm_wrdata;
+    wire        spibm_wrdata_en;
+    wire        spibm_en;
 
     wire       reset_req;
     wire       vga_vblank;
@@ -226,10 +227,16 @@ module top(
     //////////////////////////////////////////////////////////////////////////
     // System ROM
     //////////////////////////////////////////////////////////////////////////
+    wire rom_p2_wren;
+
     rom rom(
         .clk(sysclk),
         .addr({reg_bank_page[1:0], ebus_a[13:0]}),
-        .rddata(rddata_rom));
+        .rddata(rddata_rom),
+        
+        .p2_addr(spibm_a),
+        .p2_wrdata(spibm_wrdata),
+        .p2_wren(rom_p2_wren));
 
     //////////////////////////////////////////////////////////////////////////
     // ESP32 UART
@@ -346,7 +353,6 @@ module top(
     //////////////////////////////////////////////////////////////////////////
     wire [63:0] keys;
 
-    wire [15:0] spibm_a;
     wire        spibm_rd_n, spibm_wr_n, spibm_mreq_n, spibm_iorq_n;
     wire        spibm_busreq;
 
@@ -382,7 +388,8 @@ module top(
         .reset_req(reset_req),
         .keys(keys),
         .hctrl1(spi_hctrl1),
-        .hctrl2(spi_hctrl2));
+        .hctrl2(spi_hctrl2),
+        .rom_p2_wren(rom_p2_wren));
 
     assign esp_notify = 1'b0;
 
