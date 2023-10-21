c4490e26d75eb00699bea904f4bd82eee0a013fa
Frank van den Hoef <fvdhoef@icloud.com>
2023-10-21 20:28:49 +0200

    Reformatting.

--- SDK/asm/projects/cpm/cpmloader.asm
+++ SDK/asm/projects/cpm/cpmloader.asm
@@ -3,72 +3,73 @@
 	org $38E1
 
     ; Header and BASIC stub
-	db $FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF,$00
-    db "AQPLUS"
-	db $FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF,$00
-    db $0E,$39,$0A,$00,$DA,"14608",':',$80,$00,$00,$00
-    jp main
+	defb    $FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF,$00
+    defb    "AQPLUS"
+	defb    $FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF,$00
+    defb    $0E,$39,$0A,$00,$DA,"14608",':',$80,$00,$00,$00
+    jp      main
 
 cpm_start:
-    incbin "zout/cpm22.cim"
+    incbin  "zout/cpm22.cim"
 cpm_end:
 
 main:
     ; Enable turbo mode
     ld      a,$4
-    out     (IO_SYSCTRL), a
+    out     (IO_SYSCTRL),a
 
-    ; Clear screen
+    ; Clear text RAM
     ld      a,$61
-    out     (IO_VCTRL), a
-    ld      hl, $3000
-    ld      bc, $800
-    ld      a, ' '
+    out     (IO_VCTRL),a
+    ld      hl,$3000
+    ld      bc,$800
+    ld      a,' '
     call    _memset
 
+    ; Clear color RAM
     ld      a,$E1
-    out     (IO_VCTRL), a
-    ld      hl, $3000
-    ld      bc, $800
-    ld      a, $8F
+    out     (IO_VCTRL),a
+    ld      hl,$3000
+    ld      bc,$800
+    ld      a,$8F
     call    _memset
 
     ld      a,$61
-    out     (IO_VCTRL), a
+    out     (IO_VCTRL),a
 
     ; Map RAM in bank 3
-    ld  a, 35
-    out (IO_BANK3), a
+    ld      a,35
+    out     (IO_BANK3),a
 
     ; Clear bank 3
-    ld      hl, $C000
-    ld      bc, $4000
-    ld      a, 0
+    ld      hl,$C000
+    ld      bc,$4000
+    ld      a,0
     call    _memset
 
     ; Copy CP/M to bank 3
-    ld  de, $DC00
-    ld  hl, cpm_start
-    ld  bc, cpm_end - cpm_start
+    ld      de,$DC00
+    ld      hl,cpm_start
+    ld      bc,cpm_end - cpm_start
     ldir
 
     ; Map RAM in bank 0
-    ld  a, 32
-    out (IO_BANK0), a
+    ld      a,32
+    out     (IO_BANK0),a
 
     ; Jump to CP/M BIOS
-    jp  $F200
+    jp      $F200
 
 _memset:
     inc     c
     inc     b
     jr      .start
 .repeat:
-    ld      (hl), a
+    ld      (hl),a
     inc     hl
 .start:
     dec     c
-    jr      nz, .repeat
+    jr      nz,.repeat
     dec     b
-    jr      nz, .repeat
+    jr      nz,.repeat
     ret
