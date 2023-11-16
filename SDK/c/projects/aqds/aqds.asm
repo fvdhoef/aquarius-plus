f4cd557dd38d303f7e08e1cfbbbff1dd3f00e6aa
Frank van den Hoef <fvdhoef@icloud.com>
2023-11-16 07:15:48 +0100

    .

--- SDK/c/projects/aqds/aqds.asm
+++ SDK/c/projects/aqds/aqds.asm
@@ -1,11 +1,11 @@
     include "regs.inc"
 
-	org $38E1
+    org $38E1
 
     ; Header and BASIC stub
-	defb    $FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF,$00
+    defb    $FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF,$00
     defb    "AQPLUS"
-	defb    $FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF,$00
+    defb    $FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF,$00
     defb    $0E,$39,$0A,$00,$DA,"14608",':',$80,$00,$00,$00
     jp      main
 
@@ -38,18 +38,19 @@ main:
     ; Start stub
     ld      a,63
     out     (IO_BANK3),a
-    ld      hl,_stub_start
+    ld      hl,_jmp_app_stub_start
     ld      de,$C000
-    ld      bc,_stub_end - _stub_start
+    ld      bc,_jmp_app_stub_end - _jmp_app_stub_start
     ldir
-    jp      $C000
+    jp      _jmp_app
 
 .filename:  defb "Blaat2.txt",0
 
 
-_stub_start:
+_jmp_app_stub_start:
     phase   $C000
 
+_jmp_app:
     ld      a,60 | $40      ; Enable overlay
     out     (IO_BANK0),a
     ld      a,61
@@ -57,11 +58,10 @@ _stub_start:
     ld      a,62
     out     (IO_BANK2),a
     ld      sp,$3000
-
     jp      $100
 
     dephase
-_stub_end:
+_jmp_app_stub_end:
 
 _editor_start:
     incbin  "editor/build/editor.bin"
