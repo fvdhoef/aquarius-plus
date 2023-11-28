37ab899a8222f1729b44497bfb31f3766f10d672
Frank van den Hoef <fvdhoef@icloud.com>
2023-11-28 10:36:12 +0100

    Working on assembler.

--- SDK/c/projects/aqds/assembler/main.c
+++ SDK/c/projects/aqds/assembler/main.c
@@ -7,6 +7,8 @@
 
 #ifdef __SDCC
 #include "screen.h"
+#else
+#include <sys/stat.h>
 #endif
 
 #define MAX_FILE_DEPTH 8
@@ -16,17 +18,11 @@ struct file_ctx {
     char     path[30];
 };
 
-#if 0 // ndef __SDCC
-#define ENABLE_LISTING
-#endif
-
-#ifdef ENABLE_LISTING
-static FILE *f_list;
-static char  list_line[256];
-static char *list_p;
-#endif
-
-static int8_t          fd_out = -1;
+static int8_t          fd_out  = -1;
+static int8_t          fd_list = -1;
+static char            list_line[16 + 257];
+static uint8_t         list_byte;
+static bool            list_print_text;
 static uint8_t         outbuf[128];
 static uint8_t        *outbuf_p   = outbuf;
 static uint8_t         outbuf_idx = 0;
@@ -46,6 +42,8 @@ uint8_t                d_value;
 static bool            stop_parsing;
 static uint16_t        phase_offset;
 static bool            phase_enabled = false;
+static char            basename[32];
+static const char     *filename_asm;
 
 typedef void handler_t(void);
 
@@ -61,7 +59,7 @@ static void handler_org(void);
 static void handler_phase(void);
 static void handler_dephase(void);
 
-static handler_t *directive_handlers[] = {
+static handler_t *directive_handlers[TOK_DIR_LAST + 1] = {
     handler_unknown,
     handler_defb,
     handler_defs,
@@ -243,6 +241,7 @@ static uint8_t tokenize_keyword(const char *keyword) {
     if (keyword_len < 2 || keyword_len > 7)
         return TOK_UNKNOWN;
 
+    // Binary search
     uint8_t        num_elems  = num_keywords[keyword_len - 2];
     const uint8_t *elems_base = keywords[keyword_len - 2];
     uint8_t        elem_size  = keyword_len + 1;
@@ -250,13 +249,13 @@ static uint8_t tokenize_keyword(const char *keyword) {
     for (uint8_t lim = num_elems; lim != 0; lim >>= 1) {
         const uint8_t *p   = elems_base + (lim >> 1) * elem_size;
         int            cmp = keyword_cmp((const uint8_t *)keyword, p, keyword_len);
-        if (cmp == 0) {
+        if (cmp == 0)
             return p[keyword_len];
-        }
-        if (cmp > 0) { // key > p: move right
+
+        if (cmp > 0) {
             elems_base = p + elem_size;
             lim--;
-        } // else move left
+        }
     }
     return TOK_UNKNOWN;
 }
@@ -269,14 +268,55 @@ static void write_outbuf(void) {
     outbuf_p   = outbuf;
 }
 
+static void list_output_line(void) {
+    if (cur_pass != 1)
+        return;
+
+    if (!list_line[0])
+        return;
+
+    // Terminate list line with newline
+    int len          = strlen(list_line);
+    list_line[len++] = '\n';
+    list_line[len]   = 0;
+
+    // Output line to file
+    esp_write(fd_list, list_line, len);
+
+    // Clear list line
+    list_line[0]    = 0;
+    list_byte       = 0;
+    list_print_text = false;
+}
+
+static void write_hex(char *p, uint8_t val) {
+    static char hex[] = "0123456789ABCDEF";
+    p[0]              = hex[val >> 4];
+    p[1]              = hex[val & 0xF];
+}
+
+static void list_write_addr(uint16_t val) {
+    if (cur_pass != 1)
+        return;
+
+    write_hex(list_line + 0, val >> 8);
+    write_hex(list_line + 2, val);
+}
+
 static void emit_byte(uint16_t val) {
     if ((val & 0xFF00) != 0)
         error("Invalid byte value");
 
-    if (cur_pass == 1) {
-#ifdef ENABLE_LISTING
-        list_p += sprintf(list_p, "%02X", val);
-#endif
+    if (fd_list >= 0) {
+        if (list_line[0] == 0) {
+            memset(list_line, ' ', 6);
+            memset(list_line + 6, 0, 10);
+        }
+
+        write_hex(list_line + 6 + list_byte * 2, val);
+        if (++list_byte == 4) {
+            list_output_line();
+        }
 
         *(outbuf_p++) = val;
         if (++outbuf_idx == sizeof(outbuf)) {
@@ -292,6 +332,7 @@ static void handler_unknown(void) {
     syntax_error();
 }
 static void handler_defb(void) {
+    list_write_addr(cur_addr);
     skip_whitespace();
 
     while (1) {
@@ -312,6 +353,7 @@ static void handler_defb(void) {
     }
 }
 static void handler_defw(void) {
+    list_write_addr(cur_addr);
     skip_whitespace();
 
     while (1) {
@@ -326,6 +368,7 @@ static void handler_defw(void) {
     }
 }
 static void handler_defs(void) {
+    list_write_addr(cur_addr);
     uint16_t length = parse_expression(false);
     while (length--) {
         emit_byte(0);
@@ -338,10 +381,13 @@ static void handler_end(void) {
 static void handler_equ(void) {
     if (!label)
         error("Equ without label");
-    symbol_add(label, 0, parse_expression(false));
+    uint16_t value = parse_expression(false);
+    symbol_add(label, 0, value);
     expect_end_of_line();
+    list_write_addr(value);
 }
 static void handler_incbin(void) {
+    list_write_addr(cur_addr);
     parse_string();
     expect_end_of_line();
     printf("  - Processing binary file: '%s'\n", string);
@@ -361,13 +407,26 @@ static void handler_incbin(void) {
     }
     esp_close(fd);
 }
+
+static void list_path(const char *path) {
+    if (fd_list >= 0) {
+        sprintf(list_line, "**** %s ****\n", path);
+        esp_write(fd_list, list_line, strlen(list_line));
+        list_line[0] = 0;
+    }
+}
+
 static void handler_include(void) {
     parse_string();
     expect_end_of_line();
 
     printf("  - Processing include file: '%s'\n", string);
+    list_output_line();
+    list_path(string);
     parse_file(string);
+    list_path(cur_file_ctx->path);
 }
+
 static void handler_org(void) {
     uint16_t addr = parse_expression(false);
     expect_end_of_line();
@@ -384,6 +443,8 @@ static void handler_org(void) {
             }
         }
     }
+
+    list_write_addr(cur_addr);
     // printf("[Org addr: $%04x]\n", addr);
 }
 static void handler_phase(void) {
@@ -396,8 +457,10 @@ static void handler_phase(void) {
     phase_enabled = true;
     phase_offset  = cur_addr - addr;
     cur_addr      = addr;
+    list_write_addr(cur_addr);
 }
 static void handler_dephase(void) {
+    list_write_addr(cur_addr);
     expect_end_of_line();
     if (!phase_enabled)
         error("Dephase outside phase");
@@ -436,21 +499,140 @@ static char *parse_argument(void) {
     return result;
 }
 
-static void parse_file(const char *path) {
-    stop_parsing = false;
+static void parse_line(void) {
+    label           = NULL;
+    keyword         = NULL;
+    cur_p           = linebuf;
+    list_print_text = true;
 
+    // Early bail out if empty line
+    if (cur_p[0] == 0 || cur_p[0] == ';' || cur_p[0] == '\r' || cur_p[0] == '\n')
+        return;
+
+    // Check for label
+    if (cur_p[0] != ' ' && cur_p[0] != '\t')
+        parse_label();
+
+    // Check for keyword
+    parse_keyword();
+
+    // Tokenize keyword
+    uint8_t token = TOK_UNKNOWN;
+    if (keyword)
+        token = tokenize_keyword(keyword);
+
+    // First handle equ directive instead of creating normal label
+    if (token == TOK_EQU) {
+        directive_handlers[token]();
+        return;
+    }
+
+    // Add symbol for label
+    if (label) {
+        symbol_add(label, 0, cur_addr);
+        if (fd_list >= 0) {
+            list_write_addr(cur_addr);
+        }
+    }
+
+    // End of line?
+    if (!keyword && cur_p[0] == 0)
+        return;
+
+    // Directive?
+    if (token <= TOK_DIR_LAST) {
+        directive_handlers[token]();
+        expect_end_of_line();
+        return;
+    }
+
+    // Parse arguments
+    char *arg1 = parse_argument();
+    char *arg2 = parse_argument();
+    expect_end_of_line();
+
+    // Iterate over opcode info entries to find a matching entry
+    const uint8_t *info_next   = opcode_info[token - TOK_OPCODE_FIRST];
+    uint8_t        prefix_type = 0;
+    bool           found       = false;
+    while (info_next) {
+        // Extra information from current entry
+        const uint8_t *info = info_next;
+        info_next           = (info[0] & 0x80) ? (info + 3) : NULL;
+        uint8_t arg1_type   = info[0] & 0x3F;
+        uint8_t arg2_type   = info[1] & 0x3F;
+        prefix_type         = info[1] >> 6;
+        cur_opcode          = info[2];
+        cur_outtype         = OT_NONE;
+
+        // Check if arguments type matches
+        if (match_argtype(arg1, arg1_type) && match_argtype(arg2, arg2_type)) {
+            found = true;
+            break;
+        }
+    }
+    if (!found)
+        syntax_error();
+
+    list_write_addr(cur_addr);
+
+    // Emit IX/IY specific prefix
+    switch (cur_outtype & 0xC0) {
+        case OT_PREFIX_DD: emit_byte(0xDD); break;
+        case OT_PREFIX_FD: emit_byte(0xFD); break;
+        default: break;
+    }
+
+    // Emit prefix
+    switch (prefix_type) {
+        case OD_PREFIX_CB: emit_byte(0xCB); break;
+        case OD_PREFIX_ED: emit_byte(0xED); break;
+        default: break;
+    }
+
+    // (IX+d)/(IY+d) specific encoding?
+    if (prefix_type == OD_PREFIX_CB && (cur_outtype & OT_D)) {
+        emit_byte(d_value);
+        emit_byte(cur_opcode);
+        return;
+    }
+
+    // Emit opcode
+    emit_byte(cur_opcode);
+
+    // Emit displacement
+    if (cur_outtype & OT_D)
+        emit_byte(d_value);
+
+    // Emit argument
+    switch (cur_outtype & 0x1F) {
+        case OT_NONE: break;
+        case OT_8BIT: emit_byte(arg_value); break;
+        case OT_16BIT:
+            emit_byte(arg_value & 0xFF);
+            emit_byte(arg_value >> 8);
+            break;
+        default: error("Invalid outtype");
+    }
+}
+
+static void parse_file(const char *path) {
+    // Open file and check for success
     int8_t fd = esp_open(path, FO_RDONLY);
     check_esp_result(fd);
 
-    if (cur_file_ctx != NULL) {
-        file_ctx_idx++;
-        if (file_ctx_idx >= MAX_FILE_DEPTH) {
-            error("Max file depth reached");
-        }
-    }
-    cur_file_ctx         = &file_ctxs[file_ctx_idx];
-    cur_file_ctx->linenr = 0;
+    // Keep track of file context for error reporting (line number and file path)
     {
+        if (cur_file_ctx != NULL) {
+            file_ctx_idx++;
+            if (file_ctx_idx >= MAX_FILE_DEPTH) {
+                error("Max file depth reached");
+            }
+        }
+        cur_file_ctx         = &file_ctxs[file_ctx_idx];
+        cur_file_ctx->linenr = 0;
+
+        // Copy path into file context
         char *p = cur_file_ctx->path;
         for (uint8_t i = 0; i < sizeof(cur_file_ctx->path) - 1; i++) {
             char ch = path[i];
@@ -461,131 +643,35 @@ static void parse_file(const char *path) {
         *p = 0;
     }
 
+    // Parse file
+    stop_parsing = false;
     while (!stop_parsing) {
-#ifdef ENABLE_LISTING
-        list_p  = list_line;
-        *list_p = 0;
-#endif
-
         cur_file_ctx->linenr++;
+
+        // Read one line of text
         int linelen = esp_readline(fd, linebuf, 256);
         if (linelen == ERR_EOF)
             break;
         check_esp_result(linelen);
 
-        label      = NULL;
-        keyword    = NULL;
-        cur_p      = linebuf;
-        uint8_t ch = *cur_p;
-        if (ch == 0 || ch == ';' || ch == '\r' || ch == '\n')
-            continue;
-        if (ch != ' ' && ch != '\t')
-            parse_label();
-
-        // Check for keyword
-        parse_keyword();
-        uint8_t token = TOK_UNKNOWN;
-        if (keyword) {
-            token = tokenize_keyword(keyword);
-        }
-        if (token == TOK_EQU) {
-            directive_handlers[token]();
-            continue;
-        }
-        if (label)
-            symbol_add(label, 0, cur_addr);
-        if (!keyword && *cur_p == 0)
-            continue;
-
-        if (token <= TOK_DIR_LAST) {
-            directive_handlers[token]();
-            if (token == TOK_INCLUDE)
-                continue;
-
-        } else {
-            const uint8_t *info_next = opcode_info[token - TOK_OPCODE_FIRST];
-            char          *arg1      = parse_argument();
-            char          *arg2      = parse_argument();
-            expect_end_of_line();
-
-            bool done = false;
-
-#ifdef ENABLE_LISTING
-            list_p += sprintf(list_p, "%04X  ", cur_addr);
-#endif
-
-            // printf("- %s:%u: %s %s%s%s\n", cur_file_ctx->path, cur_file_ctx->linenr, keyword, arg1, arg2[0] ? "," : "", arg2);
-
-            while (info_next) {
-                const uint8_t *info = info_next;
-                info_next           = (info[0] & 0x80) ? (info + 3) : NULL;
-                uint8_t arg1_type   = info[0] & 0x3F;
-                uint8_t arg2_type   = info[1] & 0x3F;
-                uint8_t prefix_type = info[1] >> 6;
-                cur_opcode          = info[2];
-                if (arg1_type == 0x3F)
-                    error("Unimplemented opcode");
-
-                cur_outtype = OT_NONE;
-                // printf("  - arg1:%2u arg2:%2u prefix:%u opcode:$%02X\n", arg1_type, arg2_type, prefix_type, cur_opcode);
-
-                if (!match_argtype(arg1, arg1_type) || !match_argtype(arg2, arg2_type))
-                    continue;
-
-                switch (cur_outtype & 0xC0) {
-                    case OT_PREFIX_DD: emit_byte(0xDD); break;
-                    case OT_PREFIX_FD: emit_byte(0xFD); break;
-                    default: break;
-                }
-                switch (prefix_type) {
-                    case OD_PREFIX_CB: emit_byte(0xCB); break;
-                    case OD_PREFIX_ED: emit_byte(0xED); break;
-                    default: break;
-                }
-                if (prefix_type == OD_PREFIX_CB && (cur_outtype & OT_D)) {
-                    emit_byte(d_value);
-                    emit_byte(cur_opcode);
-                } else {
-                    emit_byte(cur_opcode);
-                    if (cur_outtype & OT_D) {
-                        emit_byte(d_value);
-                    }
-                    switch (cur_outtype & 0x1F) {
-                        case OT_NONE: break;
-                        case OT_8BIT: emit_byte(arg_value); break;
-                        case OT_16BIT:
-                            emit_byte(arg_value & 0xFF);
-                            emit_byte(arg_value >> 8);
-                            break;
-                        default: error("Invalid outtype");
-                    }
-                }
-
-                done = true;
-                break;
-            }
-            if (!done) {
-                syntax_error();
-            }
-
-#ifdef ENABLE_LISTING
-            while (list_p - list_line < 14) {
-                *(list_p++) = ' ';
-            }
-            *(list_p++) = '\t';
-            list_p += sprintf(list_p, "    %s%s%s%s%s", keyword, arg1[0] ? " " : "", arg1, arg2[0] ? "," : "", arg2);
-#endif
+        if (fd_list >= 0) {
+            // Copy line into listing line
+            memset(list_line, ' ', 16);
+            strcpy(list_line + 16, linebuf);
         }
 
-#ifdef ENABLE_LISTING
-        *(list_p++) = '\n';
-        *list_p     = 0;
-        fputs(list_line, f_list);
-#endif
+        // Parse the line
+        parse_line();
+
+        if (fd_list >= 0) {
+            list_output_line();
+        }
     }
 
+    // Close file
     esp_close(fd);
 
+    // Pop file context
     if (file_ctx_idx == 0) {
         cur_file_ctx = NULL;
     } else {
@@ -593,47 +679,142 @@ static void parse_file(const char *path) {
     }
 }
 
-int main(void) {
+void determine_basename(const char *path) {
+    filename_asm            = NULL;
+    int         len         = strlen(path);
+    const char *p           = path + len;
+    const char *base_startp = NULL;
+    const char *base_endp   = NULL;
+
+    // Find start of extension
+    while (p != path) {
+        p--;
+        if (*p == '/' || *p == '\\' || *p == '.')
+            break;
+    }
+    if (p == path) {
+        // No extension, no directories
+        base_startp = path;
+        base_endp   = path + len;
+    } else if (*p == '/' || *p == '\\') {
+        // No extension, has directories
+        base_startp = p + 1;
+        base_endp   = path + len;
+    } else if (*p == '.') {
+        base_endp = p;
+        while (p != path) {
+            p--;
+            if (*p == '/' || *p == '\\') {
+                base_startp = p + 1;
+                break;
+            }
+        }
+        if (!base_startp) {
+            base_startp = path;
+        }
+    }
+
+    filename_asm = base_startp;
+    p            = base_startp;
+    char *pd     = basename;
+    for (int i = 0; i < (int)sizeof(basename) - 1; i++) {
+        if (p < base_endp) {
+            *(pd++) = *(p++);
+        }
+    }
+    *pd = 0;
+
+    p  = path;
+    pd = linebuf;
+    while (p < base_startp) {
+        *(pd++) = *(p++);
+    }
+    *pd = 0;
+}
+
+int main(
 #ifdef __SDCC
+    void
+#else
+    int argc, const char **argv
+#endif
+) {
+#ifdef __SDCC
+    // Initialize screen output
     scr_init();
 #endif
+    const char *path;
 
 #ifndef __SDCC
-    // const char *path = "goaqms.asm";
-    const char *path = "all_opcodes.asm";
+    // Get path from command line argument
+    if (argc != 2) {
+        printf("Syntax: assembler <inputfile.asm>\n");
+        exit(1);
+    }
+    path = argv[1];
 #else
-    const char *path = (const char *)0xFF00;
+    // Path is located in buffer at $FF00
+    path = (const char *)0xFF00;
 #endif
 
+    // Determine base name (filename without extension) and path of assembler file (temporarily stored in linebuf)
+    determine_basename(path);
+
+    // Output header
     puts("Aquarius+ Development Studio - Z80 Assembler V1.0 by Frank van den Hoef\n");
     printf("Assembling %s\n", path);
 
+    // Change directory
+    if (*linebuf) {
+        printf("- Changing directory to: %s\n", linebuf);
 #ifndef __SDCC
-    chdir("testdata");
+        chdir(linebuf);
 #else
-    esp_chdir("/testdata");
+        esp_chdir(linebuf);
 #endif
-
-#ifdef ENABLE_LISTING
-    f_list = fopen("listing.txt", "wt");
-#endif
-    fd_out = esp_open("result.bin", FO_CREATE | FO_TRUNC | FO_WRONLY);
-    for (cur_pass = 0; cur_pass < 2; cur_pass++) {
-        printf("- Pass %d\n", cur_pass + 1);
-        cur_addr  = 0;
-        cur_scope = 0;
-        parse_file(path);
     }
 
-#ifdef ENABLE_LISTING
-    fclose(f_list);
+    // Create output directory
+    printf("- Creating output directory: out\n");
+#ifndef __SDCC
+    mkdir("out", 0777);
+#else
+    esp_mkdir("out");
 #endif
 
+    // Open output binary
+    sprintf(linebuf, "out/%s.aqx", basename);
+    printf("- Opening output binary: %s\n", linebuf);
+    fd_out = esp_open(linebuf, FO_CREATE | FO_TRUNC | FO_WRONLY);
+    check_esp_result(fd_out);
+
+    // Perform the two assembly passes
+    for (cur_pass = 0; cur_pass < 2; cur_pass++) {
+        printf("- Assembly pass %d\n", cur_pass + 1);
+
+        // Open listing
+        if (cur_pass == 1) {
+            sprintf(linebuf, "out/%s.lst", basename);
+            printf("  - Opening listing: %s\n", linebuf);
+            fd_list = esp_open(linebuf, FO_CREATE | FO_TRUNC | FO_WRONLY);
+            check_esp_result(fd_list);
+        }
+
+        cur_addr  = 0;
+        cur_scope = 0;
+        parse_file(filename_asm);
+    }
+
+    // Write any left over part to output binary and close it
     write_outbuf();
     esp_close(fd_out);
 
-    puts("Done!\n");
+    // Close listing file
+    if (fd_list >= 0)
+        esp_close(fd_list);
 
+    // We're done, exit the program
+    puts("Done!\n");
     exit_program();
     return 0;
 }
