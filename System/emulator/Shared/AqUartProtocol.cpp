73679d8427f2b0b48e6a3329d9611e40c0ccbe2d
Frank van den Hoef <fvdhoef@icloud.com>
2024-04-23 13:29:24 +0200

    plusBASIC to v0.22l

--- System/emulator/Shared/AqUartProtocol.cpp
+++ System/emulator/Shared/AqUartProtocol.cpp
@@ -23,31 +23,35 @@ static const char *TAG = "AqUartProtocol";
 #endif
 
 enum {
-    ESPCMD_RESET       = 0x01, // Indicate to ESP that system has been reset
-    ESPCMD_VERSION     = 0x02, // Get version string
-    ESPCMD_GETDATETIME = 0x03, // Get current date/time
-    ESPCMD_KEYMODE     = 0x08, // Set keyboard mode
-    ESPCMD_GETMOUSE    = 0x0C, // Get mouse state
-    ESPCMD_OPEN        = 0x10, // Open / create file
-    ESPCMD_CLOSE       = 0x11, // Close open file
-    ESPCMD_READ        = 0x12, // Read from file
-    ESPCMD_WRITE       = 0x13, // Write to file
-    ESPCMD_SEEK        = 0x14, // Move read/write pointer
-    ESPCMD_TELL        = 0x15, // Get current read/write
-    ESPCMD_OPENDIR     = 0x16, // Open directory
-    ESPCMD_CLOSEDIR    = 0x17, // Close open directory
-    ESPCMD_READDIR     = 0x18, // Read from directory
-    ESPCMD_DELETE      = 0x19, // Remove file or directory
-    ESPCMD_RENAME      = 0x1A, // Rename / move file or directory
-    ESPCMD_MKDIR       = 0x1B, // Create directory
-    ESPCMD_CHDIR       = 0x1C, // Change directory
-    ESPCMD_STAT        = 0x1D, // Get file status
-    ESPCMD_GETCWD      = 0x1E, // Get current working directory
-    ESPCMD_CLOSEALL    = 0x1F, // Close any open file/directory descriptor
-    ESPCMD_OPENDIR83   = 0x20, // Open directory in 8.3 mode
-    ESPCMD_READLINE    = 0x21, // Read line from file
-    ESPCMD_OPENDIREXT  = 0x22, // Open directory with extended options
-    ESPCMD_LOADFPGA    = 0x40, // Load FPGA bitstream
+    ESPCMD_RESET             = 0x01, // Indicate to ESP that system has been reset
+    ESPCMD_VERSION           = 0x02, // Get version string
+    ESPCMD_GETDATETIME       = 0x03, // Get current date/time
+    ESPCMD_KEYMODE           = 0x08, // Set keyboard mode
+    ESPCMD_GETMOUSE          = 0x0C, // Get mouse state
+    ESPCMD_OPEN              = 0x10, // Open / create file
+    ESPCMD_CLOSE             = 0x11, // Close open file
+    ESPCMD_READ              = 0x12, // Read from file
+    ESPCMD_WRITE             = 0x13, // Write to file
+    ESPCMD_SEEK              = 0x14, // Move read/write pointer
+    ESPCMD_TELL              = 0x15, // Get current read/write
+    ESPCMD_OPENDIR           = 0x16, // Open directory
+    ESPCMD_CLOSEDIR          = 0x17, // Close open directory
+    ESPCMD_READDIR           = 0x18, // Read from directory
+    ESPCMD_DELETE            = 0x19, // Remove file or directory
+    ESPCMD_RENAME            = 0x1A, // Rename / move file or directory
+    ESPCMD_MKDIR             = 0x1B, // Create directory
+    ESPCMD_CHDIR             = 0x1C, // Change directory
+    ESPCMD_STAT              = 0x1D, // Get file status
+    ESPCMD_GETCWD            = 0x1E, // Get current working directory
+    ESPCMD_CLOSEALL          = 0x1F, // Close any open file/directory descriptor
+    ESPCMD_OPENDIR83         = 0x20, // Open directory in 8.3 mode
+    ESPCMD_READLINE          = 0x21, // Read line from file
+    ESPCMD_OPENDIREXT        = 0x22, // Open directory with extended options
+    ESPCMD_LOADFPGA          = 0x40, // Load FPGA bitstream
+    ESPCMD_WIFI_STATUS       = 0x50, // Get WiFi status
+    ESPCMD_WIFI_SCAN         = 0x51, // Scan for WiFi APs
+    ESPCMD_WIFI_CONFIG       = 0x52, // Configure WiFi
+    ESPCMD_WIFI_SET_HOSTNAME = 0x53, // Set hostname
 };
 
 #define ESP_PREFIX "esp:"
