9045cb87b2ad1c208de9c14cd5d3b7cb8a79de99
Frank van den Hoef <fvdhoef@icloud.com>
2024-08-15 08:46:56 +0200

    Fixing compatibility with GNU sed

--- System/rom_src/settings/build.sh
+++ System/rom_src/settings/build.sh
@@ -3,5 +3,7 @@
 set -e
 make
 xxd -i -n settings_aqx_start build/settings.aqx > ../../emulator/settings_aqx.h
-sed -i "" 's/unsigned char/static const uint8_t/' ../../emulator/settings_aqx.h
-sed -i "" 's/unsigned int.*$//' ../../emulator/settings_aqx.h
+sed 's/unsigned char/static const uint8_t/' ../../emulator/settings_aqx.h > tmp.h
+mv tmp.h ../../emulator/settings_aqx.h
+sed 's/unsigned int.*$//' ../../emulator/settings_aqx.h > tmp.h
+mv tmp.h ../../emulator/settings_aqx.h
