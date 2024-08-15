a22ee2ccc36f0946fb9656f964eb3a16bbaff3ab
Frank van den Hoef <fvdhoef@icloud.com>
2024-08-15 08:36:38 +0200

    Fixing compatibility with GNU sed

--- System/rom_src/generate.sh
+++ System/rom_src/generate.sh
@@ -10,7 +10,9 @@ cp settings/build/settings.aqx settings/build/settings
 romfsgen/romfsgen.py romfs.bin settings/build/settings boot/zout/boot.bin assets/default.chr assets/latin1b.chr assets/sysrom_s2.bin plusbasic/plusBasic/zout/sysrom.bin plusbasic/plusBasic/zout/ptplay.bin
 
 xxd -i -n romfs_start romfs.bin > ../emulator/romfs_contents.h
-sed -i "" 's/unsigned char/static const uint8_t/' ../emulator/romfs_contents.h
-sed -i "" 's/unsigned int.*$//' ../emulator/romfs_contents.h
+sed 's/unsigned char/static const uint8_t/' ../emulator/romfs_contents.h > tmp.h
+mv tmp.h ../emulator/romfs_contents.h
+sed 's/unsigned int.*$//' ../emulator/romfs_contents.h > tmp.h
+mv tmp.h ../emulator/romfs_contents.h
 
 echo Done.
