2338cadc08f740129c101c3d1b774d8c45d90b22
Frank van den Hoef <fvdhoef@icloud.com>
2024-10-02 18:51:52 +0200

    Removing settings app.

--- System/rom_src/settings/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-make
-xxd -i -n settings_aqx_start build/settings.aqx > ../../emulator/settings_aqx.h
-sed 's/unsigned char/static const uint8_t/' ../../emulator/settings_aqx.h > tmp.h
-mv tmp.h ../../emulator/settings_aqx.h
-sed 's/unsigned int.*$//' ../../emulator/settings_aqx.h > tmp.h
-mv tmp.h ../../emulator/settings_aqx.h
