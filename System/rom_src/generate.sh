d327159b9b4636aca041c01cc8788d165ace0e22
Frank van den Hoef <fvdhoef@icloud.com>
2025-04-06 11:53:21 +0200

    Updates.

--- System/rom_src/generate.sh
+++ System/rom_src/generate.sh
@@ -7,11 +7,12 @@ make -C boot
 
 romfsgen/romfsgen.py romfs.bin boot/zout/boot.bin assets/default.chr assets/latin1b.chr assets/sysrom_s2.bin plusbasic/plusBasic/zout/sysrom.bin plusbasic/plusBasic/zout/ptplay.bin
 
-xxd -i -n romfs_start romfs.bin > ../emulator/romfs_contents.h
-sed 's/unsigned char/static const uint8_t/' ../emulator/romfs_contents.h > tmp.h
-mv tmp.h ../emulator/romfs_contents.h
-sed 's/unsigned int.*$//' ../emulator/romfs_contents.h > tmp.h
-mv tmp.h ../emulator/romfs_contents.h
+ROMFS_H=../emulator/esp32/romfs_contents.h
+xxd -i -n romfs_start romfs.bin > $ROMFS_H
+sed 's/unsigned char/static const uint8_t/' $ROMFS_H > tmp.h
+mv tmp.h $ROMFS_H
+sed 's/unsigned int.*$//' $ROMFS_H > tmp.h
+mv tmp.h $ROMFS_H
 
 cp romfs.bin ../esp32/main/assets/
 
